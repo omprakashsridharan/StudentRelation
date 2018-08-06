97e4c530ed7f729bfdcb1cb721c2d3aabd1b7e38
Fathima <wfathima@lm-maa-22004974.paypalcorp.com>
2018-08-06 10:37:54 +0530

    read file

--- src/Student.java
+++ src/Student.java
@@ -1,27 +1,31 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
 
 public class Student {
-    private String name;
-    private ArrayList<Integer> marks;
-
-    public Student(String name, ArrayList<Integer> marks) {
-        this.name = name;
-        this.marks = marks;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ArrayList<Integer> getMarks() {
-        return marks;
-    }
-
-    public void setMarks(ArrayList<Integer> marks) {
-        this.marks = marks;
+    public static  void main(String args[]) throws IOException {
+        HashMap<String, ArrayList<Integer>> hmap= new HashMap<String,ArrayList<Integer>>();
+        File file = new File("src/data.txt");
+        Scanner sc = new Scanner(file);
+        String index;
+        String key="0";
+        while (sc.hasNextLine())
+        {
+            ArrayList<Integer> arrli = new ArrayList<Integer>();
+            index=sc.nextLine();
+            System.out.println(index);
+            String[] array =index.split(" ");
+            key=array[0];
+            for(int i=1;i<array.length;i++)
+            {
+                arrli.add(Integer.parseInt(array[i]));
+            }
+            hmap.put(key,arrli);
+        }
     }
 }
